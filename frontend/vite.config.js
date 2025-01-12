515493f9986553c778b770bc31385351eae8c423
Prajapati Mihirkumar Mukeshkumar <127576057+Mihir563@users.noreply.github.com>
2025-01-12 11:12:01 +0530

    Update vite.config.js
    update Vite.config.js again

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // The output directory for the production build
+    outDir: 'frontend/dist',  // Ensure the build output is directed here
   },
 })
