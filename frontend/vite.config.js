78e08f55f5454a244842b75ad6f18ad47d5dbe31
Mihir563 <prajapatimihir563@gmail.com>
2025-01-12 17:46:38 +0530

    added the url again frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';  // Make sure this is imported!
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   build: {
     outDir: 'dist', // Ensure the output directory is 'dist'
   },
